73576a1c4b7a74ef2e0fcf8f962cd667659007dd
Philippe <philippe.deoliveira75@gmail.com>
2023-01-09 08:09:11 +0100

    lodging 65%

--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -8,45 +8,56 @@ import '../../index.css';
 
 function Gallery(props) {
 
-	const [current, setCurrent] = useState(0);
-
-	const pictures = props.pictures;
-
-    const length = pictures.length;
-
-    const rightSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
-    };
-
-    const leftSlide = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1);
-    };
-
-    return (
-        <>
-            {pictures.map((picture, index) =>
-                current === index && (
-                    <div key={picture} className="carrousel_pictures">
-                        <span className="carrousel_count_display">
-                            {index + 1}/{length}
-                        </span>
-                        <img src={picture} alt="Rental" />
-                        {length > 1 ? (
-                            <>
-                                <div className="carrousel_leftArrow" onClick={leftSlide}>
-                                    <img src={chevronLeft} alt="Preview arrow" />
-                                </div>
-                                <div className="carrousel_rightArrow" onClick={rightSlide}>
-                                    <img src={chevronRight} alt="Next arrow" />
-                                </div>
-                            </>
-                        ) : null}
-                    </div>
-                )
-            )}
-        </>
-    );
-
-}
+        const pictures = props.pictures;
+        const lenghtPictureList = (props.files).length;
+        let styleDisplay;
+    
+        
+        //si le logement n'a qu'une seule image les chevrons et le numéro de photo ne s'affichent pas
+        lenghtPictureList === 1 ? styleDisplay = "none" : styleDisplay = "flex";
+    
+        let [index, setIndex] = useState(0);
+    
+        function chevronNext(){
+            setIndex((curIndex) =>
+                curIndex === lenghtPictureList - 1 ? 0 : curIndex + 1
+            )
+        }
+    
+        function chevronPrevious(){
+            setIndex((curIndex) =>
+                curIndex === 0 ? lenghtPictureList - 1 : curIndex -1
+            )
+    
+        }
+    
+      return (
+            <div className="slideShowDiv" >
+                <div
+                    className="slideshowSlider"
+                    style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+                >
+                    {pictures.map((infos, index) => (
+                        <div
+                            className="slideShowImg"
+                            key={`${infos[index]}-${index}`}
+                        > 
+                            <img src={props.files[index]} alt="Slideshow Rental" />
+    
+                            <div className='number' style={{display : styleDisplay}}>
+                                {index+1 + " / " + lenghtPictureList}
+                            </div>
+                        
+                        </div>
+                    ))}
+                </div>
+                
+                <div className='changePicture' style={{display : styleDisplay}}  >
+                  <p className='previous' onClick={chevronPrevious}> <img src={chevronLeft} alt="Chevron left" /> </p>
+                  <p className='next' onClick={chevronNext}> <img src={chevronRight} alt="Chevron right" /> </p> 
+                </div>
+            </div>
+      );
+    }
 
 export default Gallery;
